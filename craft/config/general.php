070bcfad664a8fec6219c301e54ac0c555c58819
billythekid <billy@the-kid.org>
2016-07-01 15:55:30 +0100

    initial commit

---
+++ craft/config/general.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * General Configuration
+ * All of your system's general configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+ */
+
+return array(
+    '*' => [
+        'omitScriptNameInUrls' => true,
+    ],
+
+    '.dev' => [
+        'siteUrl'              => 'http://tips.dev/',
+        'devMode'              => true,
+        'environmentVariables' => [
+            'basePath' => '/Users/billythekid/sites/billys.tips/public/',
+            'baseUrl'  => 'http://tips.dev/',
+        ],
+    ],
+
+    '.tips' => [
+        'siteUrl'              => 'http://tips.dev/',
+        'devMode'              => false,
+        'environmentVariables' => [
+            'basePath' => '/home/forge/billys.tips/public/',
+            'baseUrl'  => 'https://billys.tips/',
+        ],
+    ],
+);
