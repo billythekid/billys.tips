6becf78c20eb3388cc69df3b3e518ae487a8e198
billythekid <billy@the-kid.org>
2017-04-14 12:29:20 +0100

    update craft path?

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Path to your craft/ folder
-$craftPath = '../craft';
+$craftPath = __DIR__ . '/../craft';
 
 // Do not edit below this line
 $path = rtrim($craftPath, '/') . '/app/index.php';
