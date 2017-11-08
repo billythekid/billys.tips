9fc0280e4a0ca690bd86aa0fdd655a42ef96f989
Billy Fagan <billy@fortytwo.studio>
2017-11-08 17:17:42 +0000

    remove all that shit from the server

--- craft/config/general.php
+++ craft/config/general.php
@@ -6,7 +6,6 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
-header("Access-Control-Allow-Origin: *");
 $secretKey = '33a6306c-a1e7-2656-4daa-76689d017c36';
 
 return array(
