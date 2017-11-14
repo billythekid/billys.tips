de1145e817209b29d6ccfde4e1b133310d8c5734
Billy Fagan <billy@fortytwo.studio>
2017-11-14 11:59:04 +0000

    maybe this;ll work right?

--- public/index.php
+++ public/index.php
@@ -1,8 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Headers: origin, x-requested-with, content-type');
-header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
-
 
 // Path to your craft/ folder
 $craftPath = __DIR__ . '/../craft';
