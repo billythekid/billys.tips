5fe077c9d1a6af8a889173e1e0df25280c9beafc
Billy Fagan <billy@fortytwo.studio>
2017-11-14 15:16:10 +0000

    debugging

--- craft/plugins/updatenotifier/controllers/UpdateNotifierController.php
+++ craft/plugins/updatenotifier/controllers/UpdateNotifierController.php
@@ -31,6 +31,24 @@ class UpdateNotifierController extends BaseController
         $allowedDomain = craft()->plugins->getPlugin('updatenotifier')->getSettings()->allowedDomain;
         $allowedDomain = !empty(trim($allowedDomain)) ? $allowedDomain : '*';
 
+        if (!function_exists('getallheaders'))  {
+            function getallheaders()
+            {
+                if (!is_array($_SERVER)) {
+                    return array();
+                }
+
+                $headers = array();
+                foreach ($_SERVER as $name => $value) {
+                    if (substr($name, 0, 5) == 'HTTP_') {
+                        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                    }
+                }
+                return $headers;
+            }
+        }
+        Craft::dd(getallheaders());
+
         header("Access-Control-Allow-Origin: {$allowedDomain}");
 
         $secretKey = craft()->plugins->getPlugin('updatenotifier')->getSettings()->secretKey;
@@ -65,4 +83,6 @@ class UpdateNotifierController extends BaseController
             $this->returnErrorJson(Craft::t('Could not fetch available updates at this time.'));
         }
     }
+
+
 }
