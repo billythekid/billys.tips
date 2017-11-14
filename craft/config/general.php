c9a88e35c9112d28e4f1a11f6c3470e1787ea063
Billy Fagan <billy@fortytwo.studio>
2017-11-14 17:16:57 +0000

    secret key config

--- craft/config/general.php
+++ craft/config/general.php
@@ -6,13 +6,13 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
-$secretKey = '33a6306c-a1e7-2656-4daa-76689d017c36';
+$secretKey = '33a6306c-a1e7-2656-4btk-76689d017c36';
 
 return array(
     '*' => [
         'omitScriptNameInUrls' => true,
         'devMode' => false,
-        'enableCsrfProtection' => (!isset($_POST['updatenotifierkey']) || $_POST['updatenotifierkey'] !== '33a6306c-a1e7-2656-4daa-76689d017c36'),
+        'enableCsrfProtection' => (!isset($_POST['updatenotifierkey']) || $_POST['updatenotifierkey'] !== $secretKey),
     ],
 
     '.dev' => [
