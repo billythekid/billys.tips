5452f9afe72c9198764f1b9dea77bd88cfabb4a3
Billy Fagan <billy@fortytwo.studio>
2017-11-14 11:49:40 +0000

    maybe this;ll work right?

--- craft/config/general.php
+++ craft/config/general.php
@@ -5,6 +5,7 @@
  * All of your system's general configuration settings go in here.
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
+header("Access-Control-Allow-Origin: *");
 
 $secretKey = '33a6306c-a1e7-2656-4daa-76689d017c36';
 
