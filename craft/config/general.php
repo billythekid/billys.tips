68cac0e117f89963f11ae789f3f8f143a7699055
billythekid <billy@the-kid.org>
2017-12-06 16:46:29 +0000

    handle year archives

--- craft/config/general.php
+++ craft/config/general.php
@@ -8,30 +8,33 @@
 
 $secretKey = '33a6306c-a1e7-2656-4btk-76689d017c36';
 
+$protocol = (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off") ? 'http:' : 'https:';
+$siteUrl  = $protocol . '//' . $_SERVER['HTTP_HOST'] . '/';
+
 return array(
     '*' => [
+        'siteUrl'              => $siteUrl,
         'omitScriptNameInUrls' => true,
-        'devMode' => false,
+        'devMode'              => false,
         'enableCsrfProtection' => (!isset($_POST['updatenotifierkey']) || $_POST['updatenotifierkey'] !== $secretKey),
+        'environmentVariables' => [
+            'baseUrl'  => $siteUrl,
+            'siteUrl'  => $siteUrl,
+        ],
     ],
 
     '.dev' => [
-        'siteUrl' => 'http://tips.dev/',
-        'devMode' => true,
+        'siteUrl'              => 'http://tips.dev/',
+        'devMode'              => true,
         'environmentVariables' => [
             'basePath' => '/Users/billythekid/sites/billys.tips/public/',
-            'baseUrl' => 'http://tips.dev/',
-            'siteUrl' => 'http://tips.dev/',
         ],
     ],
 
     '.tips' => [
-        'siteUrl' => 'https://billys.tips/',
+        'siteUrl'              => 'https://billys.tips/',
         'environmentVariables' => [
             'basePath' => '/home/forge/billys.tips/public/',
-            'baseUrl' => 'https://billys.tips/',
-            'siteUrl' => 'https://billys.tips/',
-
         ],
     ],
 );
